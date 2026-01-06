48f48c86180dc77e37a97c5de8f7f3dd3405e020
wwax926 <wwax926@gmail.com>
2026-01-06 09:20:50 +0300

    a

--- B-Care-Dashboard/src/config.js
+++
@@ -1,4 +0,0 @@
-// src/config.js
-export const API_BASE =
-  process.env.REACT_APP_API_BASE ||
-  "https://b-care-server-mohammad.onrender.com";
--- B-Care-Dashboard/src/socket.js
+++
@@ -1,8 +0,0 @@
-// src/socket.js
-import { io } from "socket.io-client";
-
-// We no longer send a JWT; just connect to the server
-const SERVER_URL = "https://b-care-server-mohammad.onrender.com";
-export const socket = io(SERVER_URL, {
-  transports: ["websocket"],
-});
